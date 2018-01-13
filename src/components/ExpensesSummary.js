5458a919022a78a19e36a0178c8f19c0970e42ba
James Gower <james_gower@outlook.com>
2018-01-13 13:43:06 +0000

    added hidden expenses total

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,14 +5,22 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal, allExpensesTotal, expenses }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
-  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  let formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  const expenseFullWord = expenses - expenseCount === 1 ? 'expense' : 'expenses';
+  const formattedHiddenExpenses = numeral((allExpensesTotal - expensesTotal) / 100).format('$0,0.00');
+  const formattedAllExpenses = numeral(allExpensesTotal / 100).format('$0,0.00');
+  const hiddenExpenseCount = expenses.length - expenseCount;
   
   return (
     <div className="page-header">
       <div className="content-container">
         <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+        {expensesTotal !== allExpensesTotal && 
+          <h3 className="page-header__subtitle">There {hiddenExpenseCount === 1 ? 'is ' : 'are '} 
+          <span>{hiddenExpenseCount}</span> hidden {hiddenExpenseCount === 1 ? 'expense ' : 'expenses '} 
+          totalling <span>{formattedHiddenExpenses}</span></h3>}
         <div className="page-header__actions">
           <Link className="button" to="/create">Add Expense</Link>
         </div>
@@ -21,12 +29,18 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   );
 };
 
+const resetFilter = () => {
+  expensesTotal = allExpensesTotal;
+};
+
 const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
 
   return {
     expenseCount: visibleExpenses.length,
-    expensesTotal: selectExpensesTotal(visibleExpenses)
+    expensesTotal: selectExpensesTotal(visibleExpenses),
+    allExpensesTotal: selectExpensesTotal(state.expenses),
+    expenses: state.expenses
   };
 };
 
--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount, ReactWrapper } from 'enzyme';
 import expenses from '../fixtures/expenses';
 import { EditExpensePage } from '../../components/EditExpensePage';
 import Modal from 'react-modal';
@@ -7,43 +7,39 @@ import Modal from 'react-modal';
 let startEditExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
-  startEditExpense = jest.fn();
-  startRemoveExpense = jest.fn();
-  history = { push: jest.fn() };
-  wrapper = shallow(
-
-    <EditExpensePage
-      startEditExpense={startEditExpense}
-      startRemoveExpense={startRemoveExpense}
-      history={history}
-      expense={expenses[2]}
-    />
-  );
+	startEditExpense = jest.fn();
+	startRemoveExpense = jest.fn();
+	history = { push: jest.fn() };
+	wrapper = shallow(
+		<EditExpensePage
+			startEditExpense={startEditExpense}
+			startRemoveExpense={startRemoveExpense}
+			history={history}
+			expense={expenses[2]}
+		/>
+	);
 });
 
 test('should render EditExpensePage', () => {
-  expect(wrapper).toMatchSnapshot();
+	expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle startEditExpense', () => {
-  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
-  expect(history.push).toHaveBeenLastCalledWith('/');
-  expect(startEditExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
+	wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
+	expect(history.push).toHaveBeenLastCalledWith('/');
+	expect(startEditExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
 });
 
-// test('should handle startRemoveExpense', () => {
-//   const startRemoveExpense = jest.fn();
-//   const closeModal = jest.fn();
-//   const wrapper2 = shallow(
-//     <EditExpensePage
-//       startEditExpense={startEditExpense}
-//       startRemoveExpense={startRemoveExpense}
-//       openModal={openModal}
-//       history={history}
-//       expense={expenses[2]}
-//       modalIsOpen={true}
-//     />);
-//   wrapper2.find('.button__success').simulate('click');
-//   expect(history.push).toHaveBeenLastCalledWith('/');
-//   expect(startRemoveExpense).toHaveBeenCalledWith(expenses[2]);
+// test('should open react-modal component', () => {
+// 	const wrapper = mount(
+//     <Modal
+//       isOpen={this.state.modalIsOpen}
+//       onRequestClose={this.closeModal}
+//       contentLabel="Remove Expense"
+//       style={customStyles}
+//     >
+// 		</Modal>
+//   );
+//   wrapper.find('.button__success').simulate('click');
+//   expect
 // });
